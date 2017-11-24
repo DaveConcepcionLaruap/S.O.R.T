f091459e2bd2697fe839dba18a75876687421ff4
Gaius Peteros <grpeteros06@gmail.com>
2017-11-24 13:59:09 -0800

    Added residual and recyclable falling objects

--- Assets/Game Assets/Scripts/Fall.cs
+++ Assets/Game Assets/Scripts/Fall.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Fall : MonoBehaviour {
 	public float delay = 0.5f; 
 	public GameObject coinGold;
-    public int hit;
+    public GameObject residual;
+    public GameObject recyclable;
+    public int obj;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +16,17 @@ public class Fall : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Spawn() {
-		Instantiate (coinGold, new Vector3 (Random.Range (-6, 6), 10, 0), Quaternion.identity);
-	}
+        
+        obj = Random.Range(0, 3);
+
+        if (obj == 0){
+            Instantiate(coinGold, new Vector3(Random.Range(-6, 6), 10, 0), Quaternion.identity);
+        }else if (obj == 1){
+            Instantiate(residual, new Vector3(Random.Range(-6, 6), 10, 0), Quaternion.identity);
+        }else if (obj == 2){
+            Instantiate(recyclable, new Vector3(Random.Range(-6, 6), 10, 0), Quaternion.identity);
+        }
+
+    }
 
 }
