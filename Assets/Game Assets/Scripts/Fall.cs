b9c261cb3eebce02e7eb9ccffe2635256efd149c
Weeesh <kusunokixrui@gmail.com>
2017-12-08 12:02:21 +0800

    12/8/2017

--- Assets/Game Assets/Scripts/Fall.cs
+++ Assets/Game Assets/Scripts/Fall.cs
@@ -14,22 +14,26 @@ public class Fall : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		delay = (float)Random.Range (0.5f,5.5f);
-
-		//delay = (int)delay;
-		InvokeRepeating ("Spawn", delay, delay);  
+		while (true) {
+			StopAllCoroutines ();
+			StartCoroutine (Wait ());
+			Spawn ();
+		}
 
 	}
 
-
 	// Update is called once per frame
+	IEnumerator Wait(){
+		yield return new WaitForSeconds (Random.Range (1.0f, 5.0f));
+	}
+
 	void Spawn() {
 		x = Monster.transform.position.x;
 		y = Monster.transform.position.y;
 		z = Monster.transform.position.z;
         obj = Random.Range(0, 3);
 
-		delay = (float)Random.Range (0.5f,7.5f);
+		delay = Random.Range (0.5f,7.5f);
         if (obj == 0){
 			Instantiate(coinGold, new Vector3(x, y, 0), Quaternion.identity);
         }else if (obj == 1){
