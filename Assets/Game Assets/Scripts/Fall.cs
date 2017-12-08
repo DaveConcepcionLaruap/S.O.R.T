987aa4cc4dbb0ea5d9fa097f43de7320a2aff78b
Weeesh <kusunokixrui@gmail.com>
2017-12-08 11:36:49 +0800

    Moving monster that also throws objects

--- Assets/Game Assets/Scripts/Fall.cs
+++ Assets/Game Assets/Scripts/Fall.cs
@@ -1,30 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Fall : MonoBehaviour {
-	public float delay = 0.5f; 
+	public float delay;
 	public GameObject coinGold;
     public GameObject residual;
     public GameObject recyclable;
-    public int obj;
+	public int obj;
+	public float x, y, z;
+	public GameObject Monster;
 
 	// Use this for initialization
 	void Start () {
+		delay = (float)Random.Range (0.5f,5.5f);
+
+		//delay = (int)delay;
 		InvokeRepeating ("Spawn", delay, delay);  
+
 	}
-	
+
+
 	// Update is called once per frame
 	void Spawn() {
-        
+		x = Monster.transform.position.x;
+		y = Monster.transform.position.y;
+		z = Monster.transform.position.z;
         obj = Random.Range(0, 3);
 
+		delay = (float)Random.Range (0.5f,7.5f);
         if (obj == 0){
-            Instantiate(coinGold, new Vector3(Random.Range(-6, 6), 10, 0), Quaternion.identity);
+			Instantiate(coinGold, new Vector3(x, y, 0), Quaternion.identity);
         }else if (obj == 1){
-            Instantiate(residual, new Vector3(Random.Range(-6, 6), 10, 0), Quaternion.identity);
+			Instantiate(residual, new Vector3(x, y, 0), Quaternion.identity);
         }else if (obj == 2){
-            Instantiate(recyclable, new Vector3(Random.Range(-6, 6), 10, 0), Quaternion.identity);
+			Instantiate(recyclable, new Vector3(x, y, 0), Quaternion.identity);
         }
 
     }
---
+++ Assets/Game Assets/Scripts/move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+	private Vector3 MovingDirection = Vector3.left;    //initial movement direction
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {    
+		UpdateMovement ();
+	}
+
+	void UpdateMovement(){
+		
+		if (this.transform.position.x > 10f) {
+			MovingDirection = Vector3.left;
+			gameObject.GetComponent<SpriteRenderer> ().flipX = false;
+
+
+		} else if (this.transform.position.x < -15f) { 
+			MovingDirection = Vector3.right;
+			gameObject.GetComponent<SpriteRenderer> ().flipX = true;
+
+		} 
+		this.transform.Translate ((MovingDirection*5) * Time.smoothDeltaTime);
+	}
+}
