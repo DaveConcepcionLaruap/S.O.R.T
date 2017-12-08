3f7c560e9b587053069be6046c3a1d8de647decc
Weeesh <kusunokixrui@gmail.com>
2017-12-08 12:51:21 +0800

    Fixed monster stuff

--- Assets/Game Assets/Scripts/Fall.cs
+++ Assets/Game Assets/Scripts/Fall.cs
@@ -4,44 +4,40 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Fall : MonoBehaviour {
-	public float delay;
+	//public float delay;
 	public GameObject coinGold;
     public GameObject residual;
     public GameObject recyclable;
-	public int obj;
+	public int obj, check;
 	public float x, y, z;
 	public GameObject Monster;
 
 	// Use this for initialization
 	void Start () {
-		while (true) {
-			StopAllCoroutines ();
-			StartCoroutine (Wait ());
-			Spawn ();
-		}
-
+		StartCoroutine (Spawn ());
 	}
 
 	// Update is called once per frame
-	IEnumerator Wait(){
-		yield return new WaitForSeconds (Random.Range (1.0f, 5.0f));
-	}
 
-	void Spawn() {
-		x = Monster.transform.position.x;
-		y = Monster.transform.position.y;
-		z = Monster.transform.position.z;
-        obj = Random.Range(0, 3);
 
-		delay = Random.Range (0.5f,7.5f);
-        if (obj == 0){
-			Instantiate(coinGold, new Vector3(x, y, 0), Quaternion.identity);
-        }else if (obj == 1){
-			Instantiate(residual, new Vector3(x, y, 0), Quaternion.identity);
-        }else if (obj == 2){
-			Instantiate(recyclable, new Vector3(x, y, 0), Quaternion.identity);
-        }
+	IEnumerator Spawn() {
 
+		while (Monster != null) {
+			x = Monster.transform.position.x;
+			y = Monster.transform.position.y;
+			z = Monster.transform.position.z;
+			obj = Random.Range (0, 3);
+
+			if (obj == 0) {
+				Instantiate (coinGold, new Vector3 (x, (y - 2), 0), Quaternion.identity);
+			} else if (obj == 1) {
+				Instantiate (residual, new Vector3 (x, (y - 2), 0), Quaternion.identity);
+			} else if (obj == 2) {
+				Instantiate (recyclable, new Vector3 (x, (y - 2), 0), Quaternion.identity);
+			}
+			int random = Random.Range (1, 4);
+			yield return new WaitForSeconds (random);
+		}
     }
 
 }
