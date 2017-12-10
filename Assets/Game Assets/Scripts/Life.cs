da0fb1b0f356613e279d688d8c4936d2f6a447b1
Gaius Peteros <grpeteros06@gmail.com>
2017-12-09 18:57:31 -0800

    Added change of colors on count for monster

--- Assets/Game Assets/Scripts/Life.cs
+++ Assets/Game Assets/Scripts/Life.cs
@@ -3,25 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Life : MonoBehaviour {
-	int count = 0;
+	public int count = 0;
 	public GameObject mons;
-	// Use this for initialization
-	void Start () {
-		
-	}
+    SpriteRenderer spriteRenderer;
+    // Use this for initialization
+    void Start () {
+        spriteRenderer = (SpriteRenderer) mons.GetComponent<Renderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if (count == 4) {
 			Destroy (mons);
 		}
-	}
 
-	void OnTriggerEnter2D(Collider2D Col){
+        spriteRenderer = (SpriteRenderer)mons.GetComponent<Renderer>();
+    }
 
+	void OnTriggerEnter2D(Collider2D Col)
+    {
+        
 		if (Col.gameObject.tag == "Power") {
 			count++;
 		}
 
+
+        UpdateMonsterColor();
 	}
+
+    void UpdateMonsterColor()
+    {
+        switch (count)
+        {
+            case 1:
+                spriteRenderer.color = new Color(0, 1, 0, 1);
+                break;
+            case 2:
+                spriteRenderer.color = new Color(255, 255, 0, 1);
+                break;
+            case 3:
+                spriteRenderer.color = new Color(255, 0, 0, 1);
+                break;
+        }
+
+    }
 }
